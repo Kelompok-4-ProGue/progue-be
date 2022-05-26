db0a9f8331230246f2678ccf954b70885f317aaa
rin4th <rizalnur.art@gmail.com>
2022-05-26 10:48:52 +0700

    create model JobFinder

---
+++ app/Http/Controllers/JobFinderController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JobFinder;
+use Illuminate\Http\Request;
+
+class JobFinderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\JobFinder  $jobFinder
+     * @return \Illuminate\Http\Response
+     */
+    public function show(JobFinder $jobFinder)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\JobFinder  $jobFinder
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(JobFinder $jobFinder)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\JobFinder  $jobFinder
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, JobFinder $jobFinder)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\JobFinder  $jobFinder
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(JobFinder $jobFinder)
+    {
+        //
+    }
+}
