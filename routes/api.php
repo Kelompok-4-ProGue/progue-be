8e7947abbd852254ef6ac9e7d8ea4b680c9d1097
Indra <iwahyu@student.telkomuniversity.ac.id>
2022-06-03 10:49:54 +0700

    added routes & endpoints

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobVacancyController;
 use App\Http\Controllers\JobFinderController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,9 +18,30 @@ use App\Http\Controllers\JobFinderController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['cors', 'json.response']], function () {
+    // Authentication
+    Route::post('/login', [AuthController::class, 'login']); // Login
+    Route::post('/register/company', [AuthController::class, 'registerCompany']); // Register Company
+    Route::post('/register/job_finder', [AuthController::class, 'registerJobFinder']); // Register Job Finder
+    
+    // All with token can access
+    Route::middleware('auth:api')->group(function() {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        }); // Check User
+
+        Route::resource('job-vacancy', JobVacancyController::class); // Job Vacancy        
+    });
+
+    // Only Company can access
+    Route::middleware(['auth:api', 'api.company'])->group(function () {
+        Route::resource('company', CompanyController::class); // Company
+    });
+
+    // Only Job Finder can access
+    Route::middleware(['auth:api', 'api.job_finder'])->group(function () {
+        Route::resource('job-finder', JobFinderController::class); // Job Finder
+    });
 });
 
-Route::resource('job-vacancy', JobVacancyController::class);
-Route::resource('job-finder', JobFinderController::class);
+
