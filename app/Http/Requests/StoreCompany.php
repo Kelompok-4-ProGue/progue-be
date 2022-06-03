0549b10ecbb0804e5e11b09489c3471bb7fc24ca
Indra <iwahyu@student.telkomuniversity.ac.id>
2022-06-03 10:48:37 +0700

    added request validator

---
+++ app/Http/Requests/StoreCompany.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCompany extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'company_name' => 'required|string|max:50',
+            'company_address' => 'required|string|max:250',
+            'company_letter' => 'sometimes|string'
+        ];
+    }
+}
